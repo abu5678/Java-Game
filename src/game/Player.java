b4fab6a7c39880575e5ab64ad6563467ff5e7841
Muhammed-Siddique <Muhammed.Siddique@city.ac.uk>
2023-03-09 19:31:17 +0000

    changed player image and polygon shape

--- src/game/Player.java
+++ src/game/Player.java
@@ -2,11 +2,16 @@ package game;
 
 import city.cs.engine.*;
 
-public class Player extends DynamicBody {
-    private static final Shape playerShape = new BoxShape(1,2);
+public class Player extends Walker {
+    private static final Shape playerShape = new PolygonShape(
+            -2.49f,-1.68f, -0.76f,-1.66f,
+            -0.24f,-1.22f, -0.14f,0.62f,
+            -1.39f,1.96f, -2.07f,1.91f,
+            -2.32f,1.63f, -2.55f,-1.59f
+    );
 
     private static final BodyImage image =
-            new BodyImage("data/student.png", 4f);
+            new BodyImage("player/idle/idle0.png", 4f);
 
     public Player(World world) {
         super(world, playerShape);
